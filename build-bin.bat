721622ce69cee4b0f958041c7e23bf0ee0d8d538
AaronCai <aaron.cai@xaiten.com>
2025-04-17 19:11:26 +0800

    ready for release

--- build-bin.bat
+++ build-bin.bat
@@ -23,41 +23,14 @@ echo -----------------------------------
 echo --------Building config.bin--------
 echo -----------------------------------
 echo `
-echo Building config.bin for board 204
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-204.cvs .\build\config-204.bin 0x6000
-
-echo Building config.bin for board 302
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-302.cvs .\build\config-302.bin 0x6000
-
-echo Building config.bin for board 303
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-303.cvs .\build\config-303.bin 0x6000
-
-echo Building config.bin for board 402
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-402.cvs .\build\config-402.bin 0x6000
-
-echo Building config.bin for board 601
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-601.cvs .\build\config-601.bin 0x6000
-
-echo Building config.bin for board 702
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-702.cvs .\build\config-702.bin 0x6000
+echo Building config.bin for board Zyber8S
+python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-zyber8s.cvs .\build\config-zyber8s.bin 0x6000
 
 
 
 
-echo Building config.bin for board 204-office
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-204-office.cvs .\build\config-204-office.bin 0x6000
-
-echo Building config.bin for board 303-office
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-303-office.cvs .\build\config-303-office.bin 0x6000
-
-echo Building config.bin for board 402-office
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-402-office.cvs .\build\config-402-office.bin 0x6000
-
-echo Building config.bin for board 601-office
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-601-office.cvs .\build\config-601-office.bin 0x6000
-
-echo Building config.bin for board 702-office
-python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-702-office.cvs .\build\config-702-office.bin 0x6000
+echo Building config.bin for board Zyber8S-office
+python.exe D:\espidf\v5.4\esp-idf\components\nvs_flash\nvs_partition_generator\nvs_partition_gen.py generate .\config-zyber8s-office.cvs .\build\config-zyber8s-office.bin 0x6000
 
 
 
@@ -66,40 +39,12 @@ echo -----------------------------------
 echo --------Building factory.bin-------
 echo -----------------------------------
 echo `
-echo Building factory.bin for board 204
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-204.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-204.bin"
+echo Building factory.bin for board zyber8s
+esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-zyber8s.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-zyber8s-factory-%VERSION%.bin"
 
-echo Building factory.bin for board 302
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-302.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-302.bin"
+echo Building factory.bin for board zyber8s-office
+esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-zyber8s-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-zyber8s-office-factory-%VERSION%.bin"
 
-echo Building factory.bin for board 303
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-303.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-303.bin"
-
-echo Building factory.bin for board 402
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-402.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-402.bin"
-
-echo Building factory.bin for board 601
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-601.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-601.bin"
-
-echo Building factory.bin for board 702
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-702.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-702.bin"
-
-
-
-echo Building factory.bin for board 204-office
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-204-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-204-office.bin"
-
-echo Building factory.bin for board 303-office
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-303-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-303-office.bin"
-
-echo Building factory.bin for board 402-office
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-402-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-402-office.bin"
-
-echo Building factory.bin for board 601-office
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-601-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-601-office.bin"
-
-echo Building factory.bin for board 702-office
-esptool.exe --chip esp32s3 merge_bin --flash_mode dio --flash_size 8MB --flash_freq 80m 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/config-702-office.bin 0x10000 build/esp-miner.bin 0x410000 build/www.bin 0xf10000 build/ota_data_initial.bin -o "bin\esp-miner-factory-%VERSION%-TCH-All-in-one-702-office.bin"
 
 xcopy %CURDIR%\build\esp-miner.bin %CURDIR%\bin /Y
 xcopy %CURDIR%\build\www.bin %CURDIR%\bin /Y
